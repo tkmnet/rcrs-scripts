c2a59817c423629e75ff7936ef5deb4e35869b9a
Shunki TAKAMI <tkmnet@users.noreply.github.com>
2015-01-15 21:05:26 +0900

    add fix-roborescue.sh

--- fix-roborescue.sh
+++ fix-roborescue.sh
@@ -60,8 +60,10 @@ $WGET https://raw.githubusercontent.com/tkmnet/rcrs-scripts/master/boot-scripts/
 $WGET https://raw.githubusercontent.com/tkmnet/rcrs-scripts/master/boot-scripts/start-precompute.sh >/dev/null 2>&1
 $WGET https://raw.githubusercontent.com/tkmnet/rcrs-scripts/master/boot-scripts/start-comprun.sh >/dev/null 2>&1
 $WGET https://raw.githubusercontent.com/tkmnet/rcrs-scripts/master/boot-scripts/kill.sh >/dev/null 2>&1
+$WGET https://raw.githubusercontent.com/tkmnet/rcrs-scripts/master/boot-scripts/print-lastscore.sh >/dev/null 2>&1
 chmod a+x start*.sh
 chmod a+x kill.sh
+chmod a+x print-lastscore.sh
 cd ..
 
 find ./ -name "build*.xml" | xargs sed -i -e 's/<javac\(.*\)>/<javac\1  encoding="UTF-8">/g'
