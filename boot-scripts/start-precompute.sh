69f422da1fc1da69f093b211bb3c8625b1529a3b
Shunki <tkmnet@users.noreply.github.com>
2015-01-15 18:39:13 +0900

    Update start-precompute.sh

--- boot-scripts/start-precompute.sh
+++ boot-scripts/start-precompute.sh
@@ -2,6 +2,8 @@
 
 cd `dirname $0`
 
+rm -f ./server.pids.tmp
+
 . functions.sh
 
 processArgs $*
@@ -12,7 +14,10 @@ rm -f $LOGDIR/*.log
 #startGIS
 startKernel --nomenu
 
+$PIDS > ./server.pids.tmp
+
 echo "Start your agents"
 waitFor $LOGDIR/kernel.log "Kernel has shut down" 30
 
+rm -f ./server.pids.tmp
 kill $PIDS
