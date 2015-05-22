c4a95a3d9d9a78c156dd95f20c0fe9a1dc973055
Shunki TAKAMI(takamin@sylph.maslab) <tkmnet@users.noreply.github.com>
2015-05-23 00:49:20 +0900

    update

--- adk-scripts/standardize/compile.sh
+++ adk-scripts/standardize/compile.sh
@@ -2,4 +2,6 @@
 
 cd `dirname $0`
 
+
+chmod a+x ./gradle/gradle-2.1/bin/gradle
 ./gradle/gradle-2.1/bin/gradle build
--- adk-scripts/standardize/precompute.sh
+++ adk-scripts/standardize/precompute.sh
@@ -2,4 +2,5 @@
 
 cd `dirname $0`
 
+chmod a+x ./gradle/gradle-2.1/bin/gradle
 ./gradle/gradle-2.1/bin/gradle -Pargs="-h:$7 -fb:$1 -fs:$2 -pf:$3 -po:$4 -at:$5 -ac:$6 -pre:true" start
