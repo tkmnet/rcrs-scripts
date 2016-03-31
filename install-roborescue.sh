1f17851a7574a2750f78c380391ff10a0c4a5075
Shunki Takami <tkmnet@users.noreply.github.com>
2016-04-01 01:04:36 +0900

    catch up to v1.2

--- install-roborescue.sh
+++ install-roborescue.sh
@@ -4,14 +4,6 @@ WGET='wget --no-check-certificate'
 if ! [ -x `which wget||echo /dev/null` ]; then
 	if [ -x `which curl||echo /dev/null` ]; then
 		WGET='curl -O'
-	else
-		if ! [ -x `which apt-get||echo /dev/null` ]; then
-			echo "[!] This script repuire apt-get."
-			exit
-		fi
-		sudo apt-get install -y wget
-		sh -c "sh $0"
-		exit
 	fi
 fi
 
@@ -19,34 +11,33 @@ if ! [ -x `which javac||echo /dev/null` ]; then
 	if [ `javac -version 2>&1 | grep -o 'javac 1.8.*' | wc -l` != 1 ]; then
 		echo "[!] This script repuire java8."
 		echo "Please install OracleJDK 8."
-		cd /tmp
-		$WGET https://raw.githubusercontent.com/tkmnet/Tools/master/install-oracle-jdk.sh
-		sh install-oracle-jdk.sh
-		sh -c "sh $0"
+#		cd /tmp
+#		$WGET https://raw.githubusercontent.com/tkmnet/Tools/master/install-oracle-jdk.sh
+#		sh install-oracle-jdk.sh
+#		sh -c "sh $0"
 		exit
 	fi
 fi
 
-if ! [ -x `which ant||echo /dev/null`  -a -x `which xterm||echo /dev/null` -a -x `which tar||echo /dev/null` -a -x `which gzip||echo /dev/null` ]; then
-	if ! [ -x `which apt-get||echo /dev/null` ]; then
-		echo "[!] This script repuire apt-get."
-		exit
-	fi
-	sudo apt-get install -y ant xterm tar gzip
-	sh -c "sh $0"
-	exit
+if ! [ -x `which xterm||echo /dev/null` -a -x `which tar||echo /dev/null` -a -x `which gzip||echo /dev/null` ]; then
+    echo "[!] This script repuire tar,gzip,xterm."
+    exit
 fi
 
-$WGET http://downloads.sourceforge.net/project/roborescue/2014/server/roborescue.tar.gz
-tar zxvf ./roborescue.tar.gz
-rm ./roborescue.tar.gz
-cd ./roborescue
+$WGET http://downloads.sourceforge.net/project/roborescue/roborescue/v1.2/roborescue-v1.2.tar.gz
+tar zxvf ./roborescue-v1.2.tar.gz
+rm ./roborescue-v1.2.tar.gz
+cd ./roborescue-v1.2
+
+$WGET 'http://ftp.tsukuba.wide.ad.jp/software/apache//ant/binaries/apache-ant-1.9.6-bin.tar.gz'
+tar zxvf ./apache-ant-1.9.6-bin.tar.gz
+rm ./apache-ant-1.9.6-bin.tar.gz
 
 $WGET https://raw.githubusercontent.com/tkmnet/rcrs-scripts/master/fix-roborescue.sh
 sh ./fix-roborescue.sh -y
 rm ./fix-roborescue.sh
 
-ant complete-build
+./apache-ant-1.9.6/bin/ant complete-build
 
 echo "Done."
 echo "RCRS Server is installed to '`pwd -P`'"
