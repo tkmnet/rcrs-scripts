56a1f72f42496914ac8c8a334113a7b77257e9b3
Shunki TAKAMI(takamin@sylph.maslab) <tkmnet@users.noreply.github.com>
2016-11-12 17:20:43 +0900

    fix

--- fix-roborescue.sh
+++ fix-roborescue.sh
@@ -71,7 +71,7 @@ chmod a+x print-lastscore.sh
 cd ..
 
 #find ./ -name "build*.xml" | xargs sed -i -e 's/<javac\(.*\)>/<javac\1  encoding="UTF-8">/g'
-find ./ -name "build*.xml" | xargs sed -i -e 's/\(.*\)\(importClass(org.apache.tools.ant.types.Path);\)/\1load("nashorn:mozilla_compat.js");
+find ./ -name "build*.xml" | xargs sed -i -e 's/\(.*\)\(importClass(org.apache.tools.ant.types.Path);\)/\1load("nashorn:mozilla_compat.js");\
 \1\2/g'
 
 
