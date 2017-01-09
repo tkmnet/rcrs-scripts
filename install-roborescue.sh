54ee7fbc4bb80298421069a0d1be71420ab00377
Shunki <tkmnet@users.noreply.github.com>
2017-01-09 20:39:02 +0900

    Update install-roborescue.sh

--- install-roborescue.sh
+++ install-roborescue.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 WGET='wget --no-check-certificate'
+WGETSOUT='wget --no-check-certificate -O -'
 if ! [ -x `which wget||echo /dev/null` ]; then
 	if [ -x `which curl||echo /dev/null` ]; then
 		WGET='curl -LO'
+		WGETSOUT='curl -L'
 	fi
 fi
 
@@ -24,7 +26,7 @@ if ! [ -x `which tar||echo /dev/null` -a -x `which gzip||echo /dev/null` ]; then
     exit
 fi
 
-#ANTVER=`wget -O - "http://ftp.riken.jp/net/apache//ant/binaries/" | grep -s 'a.* href=".*-bin\.tar\.gz"' | sed -e 's/^.* href="\(apache-ant-.*\)".*$/\1/' | head -1 | sed -e 's/-bin\.tar\.gz$//'`
+#ANTVER=`${WGETSOUT} "http://ftp.riken.jp/net/apache//ant/binaries/" | grep -s 'a.* href=".*-bin\.tar\.gz"' | sed -e 's/^.* href="\(apache-ant-.*\)".*$/\1/' | head -1 | sed -e 's/-bin\.tar\.gz$//'`
 ANTVER='apache-ant-1.10.0'
 
 $WGET http://downloads.sourceforge.net/project/roborescue/roborescue/v1.2/roborescue-v1.2.tar.gz
@@ -33,7 +35,7 @@ rm ./roborescue-v1.2.tar.gz
 cd ./roborescue-v1.2
 
 $WGET "http://ftp.riken.jp/net/apache//ant/binaries/${ANTVER}-bin.tar.gz"
-tar zxvf ./${ANTVER}bin.tar.gz
+tar zxvf ./${ANTVER}-bin.tar.gz
 rm ./${ANTVER}-bin.tar.gz
 
 $WGET https://raw.githubusercontent.com/tkmnet/rcrs-scripts/master/fix-roborescue.sh
