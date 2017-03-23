a781dee1944f96fbfe22e9e89fd12b6de6ddd4e6
Shunki <tkmnet@users.noreply.github.com>
2017-03-24 02:57:08 +0900

    Update functions.sh

--- boot-scripts/functions.sh
+++ boot-scripts/functions.sh
@@ -146,7 +146,7 @@ function startKernel {
     KERNEL_OPTIONS="-c $CONFIGDIR/kernel.cfg --gis.map.dir=$MAP --kernel.logname=$LOGDIR/rescue.log $*"
     makeClasspath $BASEDIR/jars $BASEDIR/lib
 
-    xterm -T kernel -e "/bin/sh -c 'java -Xmx2048m -cp $CP -Dlog4j.log.dir=$LOGDIR kernel.StartKernel $KERNEL_OPTIONS 2>&1 | tee $LOGDIR/kernel-out.logi'" &
+    xterm -T kernel -e "/bin/sh -c 'java -Xmx2048m -cp $CP -Dlog4j.log.dir=$LOGDIR kernel.StartKernel $KERNEL_OPTIONS 2>&1 | tee $LOGDIR/kernel-out.log'" &
     PIDS="$PIDS $!"
     # Wait for the kernel to start
     waitFor $LOGDIR/kernel.log "Listening for connections"
