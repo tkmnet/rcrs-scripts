5909b12a720554db16285eda2db6d290b4b2b9fe
Shunki Takami <tkmnet@users.noreply.github.com>
2017-05-01 03:20:39 +0900

    Update: install-roborescue.sh

--- install-roborescue.sh
+++ install-roborescue.sh
@@ -42,7 +42,8 @@ $WGET https://raw.githubusercontent.com/tkmnet/rcrs-scripts/master/fix-roborescu
 sh ./fix-roborescue.sh -y
 rm ./fix-roborescue.sh
 
-LANG=en_US.UTF-8 ./${ANTVER}/bin/ant complete-build
+export LANG=en_US.UTF-8
+./${ANTVER}/bin/ant complete-build
 
 echo "Done."
 echo "RCRS Server is installed to '`pwd -P`'"
