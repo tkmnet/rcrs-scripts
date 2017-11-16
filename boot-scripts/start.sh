ed77bd03bf4b3f080741c902833308978e463a8d
Shunki Takami <tkmnet@users.noreply.github.com>
2017-11-16 16:02:53 +0900

    Update start.sh

--- boot-scripts/start.sh
+++ boot-scripts/start.sh
@@ -14,7 +14,8 @@ startKernel --nomenu
 startSims
 
 echo "Start your agents"
-waitFor $LOGDIR/kernel.log "Kernel has shut down" 30
+waitFor $LOGDIR/kernel.log "Kernel is shutting down" 30
+timeout 30 waitFor $LOGDIR/kernel.log "Kernel has shut down" 30
 
 kill $PIDS
 sh ./kill.sh
